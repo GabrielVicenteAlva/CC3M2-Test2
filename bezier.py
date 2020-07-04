f9b9b6f580d9c5c6d1f9e36346595b9344667580
GabrielVicenteAlva <42977334+GabrielVicenteAlva@users.noreply.github.com>
2020-07-03 21:41:18 -0500

    Update bezier.py

--- bezier.py
+++ bezier.py
@@ -1,5 +1,4 @@
 import param
-import math
 
 #Puntos
 P = [(-3,0),(-1,4),(2,3),(4,1)]
@@ -12,5 +11,4 @@ def B(t):
 	-3 + 6*t + 3*t**2 - 2*t**3, \
 	12*t - 15*t**2 + 4*t**3
 
-param.param(B,0,1,100)
-
+param.param(B,0.,1.,100)
