d580f8f1d63758a4ae97146a5515fa4050c6c781
GabrielVicenteAlva <42977334+GabrielVicenteAlva@users.noreply.github.com>
2020-07-03 21:42:28 -0500

    Update param.py

--- param.py
+++ param.py
@@ -3,19 +3,21 @@ import matplotlib.pyplot as plt
 # Figura
 fig = plt.figure().gca()
 
-# Dibujar punto
+# Graficar puntos
 def points(x,y):
 	fig.scatter(x, y)
 	
-# Rango
+# Intervalo
 def linespace(mn,mx,n):
+	mn = float(mn)
+	mx = float(mx)
 	step = float(mx-mn)/n
 	while(mn<mx):
 		yield mn
 		mn += step
 	yield mx
 
-# Dibujar ecuacion parametrica
+# Graficar ecuacion parametrica
 def param(f,mn,mx,seg=100):
 	fig.plot(*zip(*(f(t) for t in linespace(mn,mx,seg))))
 	plt.show()
